905b5b0800219c57346a0e8fc4fbdf62aca51a33
Tiberiu Aurelian Țoca <78236499+ReverseZoom2151@users.noreply.github.com>
2024-03-19 20:15:46 +0000

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "hjujwlhiugapccasnhkc.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
